7d6546b06c04b5c1a7d6546c19d0ea0865231a30
shafiaansar <shafiaansar142@gmail.com>
2023-10-01 18:49:22 +0500

    Lets build Netflix

---
+++ netflix/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="bg-black h-full w-full lg:bg-opacity-50">
+      <nav className='px-12 py-5'>
+        <img src="/images/logo.png" alt="Logo"  className='h-12'/>
+      </nav>
+    </div>
+    </div>
+  )
+}
+
+export default Auth
