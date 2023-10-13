e382193d9e0c4effd67670d9043ab6da7df1ecd1
shafiaansar <shafiaansar142@gmail.com>
2023-10-13 22:49:24 +0500

    Movies Page

--- netflix-app/src/app/movies/page.js
+++ netflix-app/src/app/movies/page.js
@@ -1,20 +1,120 @@
 'use client'
 
-import { useContext } from 'react'
+import { useContext,useEffect } from 'react'
 import UnauthPage from '../../components/unauth-page/index'
 import { useSession } from "next-auth/react"
 import { GlobalContext } from '@/context'
 import ManageAccounts from '@/components/manage-accounts'
 import CommonLayout from '@/components/common-layout'
+import CircleLoader from '@/components/circle-loader'
+import { getAllfavorites, getTVorMoviesByGenre } from '@/utils'
 
 export default function Movies (){
-
-    const {loggedInAccount} = useContext(GlobalContext)
+    const {
+        loggedInAccount,
+        mediaData,
+        setMediaData,
+        setPageLoader,
+        pageLoader,
+      } = useContext(GlobalContext);
     const {data:session} = useSession()
+    useEffect(() => {
+        async function getAllMedias() {
+          const action = await getTVorMoviesByGenre("movie", 28);
+          const adventure = await getTVorMoviesByGenre("movie", 12);
+          const crime = await getTVorMoviesByGenre("movie", 80);
+          const comedy = await getTVorMoviesByGenre("movie", 35);
+          const family = await getTVorMoviesByGenre("movie", 10751);
+          const mystery = await getTVorMoviesByGenre("movie", 9648);
+          const romance = await getTVorMoviesByGenre("movie", 10749);
+          const scifiAndFantasy = await getTVorMoviesByGenre("movie", 878);
+          const war = await getTVorMoviesByGenre("movie", 10752);
+          const history = await getTVorMoviesByGenre("movie", 36);
+          const drama = await getTVorMoviesByGenre("movie", 18);
+          const thriller = await getTVorMoviesByGenre("movie", 53);
+          const horror = await getTVorMoviesByGenre("movie", 27);
+          const allFavorites = await getAllfavorites(
+            session?.user?.uid,
+            loggedInAccount?._id
+          );
+          console.log(allFavorites, 'allFavorites');
+          setMediaData(
+            [
+              {
+                title: "Action",
+                medias: action,
+              },
+              {
+                title: "Adventure",
+                medias: adventure,
+              },
+              {
+                title: "Crime",
+                medias: crime,
+              },
+              {
+                title: "Comedy",
+                medias: comedy,
+              },
+              {
+                title: "Family",
+                medias: family,
+              },
+              {
+                title: "Mystery",
+                medias: mystery,
+              },
+              {
+                title: "Horror",
+                medias: horror,
+              },
+              {
+                title: "History",
+                medias: history,
+              },
+              {
+                title: "Romance",
+                medias: romance,
+              },
+              {
+                title: "Sci-Fi and Fantasy",
+                medias: scifiAndFantasy,
+              },
+              {
+                title: "Thriller",
+                medias: thriller,
+              },
+              {
+                title: "War",
+                medias: war,
+              },
+              {
+                title: "Dramas",
+                medias: drama,
+              },
+            ].map((item) => ({
+              ...item,
+              medias: item.medias.map((mediaItem) => ({
+                ...mediaItem,
+                type: "movie",
+                addedToFavorites:
+                  allFavorites && allFavorites.length
+                    ? allFavorites.map((fav) => fav.movieID).indexOf(mediaItem.id) >
+                      -1
+                    : false,
+              })),
+            }))
+          );
+          setPageLoader(false);
+        }
+    
+        getAllMedias();
+      }, [loggedInAccount]);
     console.log(session)
     if (session == null) return <UnauthPage/>
     if(loggedInAccount == null) return <ManageAccounts/>
+    if(pageLoader) return <CircleLoader/>
     return   <main className=' flex flex-col min-h-screen'>
-    <CommonLayout/>
+    <CommonLayout mediaData={mediaData}/>
 </main>
 }
