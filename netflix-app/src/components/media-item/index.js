6a1e785ae0b972bc3cf6ef3366b76292c29d6094
shafiaansar <shafiaansar142@gmail.com>
2023-10-15 08:48:26 +0500

    Ahh!! finally it worked

--- netflix-app/src/components/media-item/index.js
+++ netflix-app/src/components/media-item/index.js
@@ -1,72 +1,187 @@
 "use client";
-
-import { GlobalContext } from "@/context";
-import { getAllfavorites } from "@/utils";
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useContext } from "react";
 import { motion } from "framer-motion";
-import Navbar from "@/components/navbar";
-import MediaItem from "@/components/media-item";
-import CircleLoader from "@/components/circle-loader";
-import UnauthPage from "@/components/unauth-page";
-import ManageAccounts from "@/components/manage-accounts";
+import Image from "next/image";
+import {
+  PlusIcon,
+  ChevronDownIcon,
+  CheckIcon,
+} from "@heroicons/react/24/outline";
+import { usePathname, useRouter } from "next/navigation";
+import { useContext } from "react";
+import { GlobalContext } from "@/context";
+import { useSession } from "next-auth/react";
+import { getAllfavorites } from "@/utils";
 
-export default function MyList() {
+const baseUrl = "https://image.tmdb.org/t/p/w500";
+
+export default function MediaItem({
+  media,
+  searchView = false,
+  similarMovieView = false,
+  listView = false,
+  title,
+}) {
+  const router = useRouter();
+  const pathName = usePathname();
   const {
-    favorites,
-    setFavorites,
-    pageLoader,
-    setPageLoader,
+    setShowDetailsPopup,
     loggedInAccount,
+    setFavorites,
+    setCurrentMediaInfoIdAndType,
+    similarMedias,
+    searchResults,
+    setSearchResults,
+    setSimilarMedias,
+    setMediaData,
+    mediaData,
   } = useContext(GlobalContext);
+
   const { data: session } = useSession();
 
-  useEffect(() => {
-    async function extractFavorites() {
-      const data = await getAllfavorites(
-        session?.user?.uid,
-        loggedInAccount?._id
+  async function updateFavorites() {
+    const res = await getAllfavorites(session?.user?.uid, loggedInAccount?._id);
+    if (res)
+      setFavorites(
+        res.map((item) => ({
+          ...item,
+          addedToFavorites: true,
+        }))
       );
+  }
 
-      console.log(data);
+  async function handleAddToFavorites(item) {
+    const { backdrop_path, poster_path, id, type } = item;
+    const res = await fetch("/api/favorites/add-favorite", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        backdrop_path,
+        poster_path,
+        movieID: id,
+        type,
+        uid: session?.user?.uid,
+        accountID: loggedInAccount?._id,
+      }),
+    });
 
-      if (data) {
-        setFavorites(data.map(item=> ({
-          ...item, addedToFavorites : true
-        })));
-        setPageLoader(false);
+    const data = await res.json();
+
+    if (data && data.success) {
+      if (pathName.includes("my-list")) updateFavorites();
+      if (searchView) {
+        let updatedSearchResults = [...searchResults];
+        const indexOfCurrentAddedMedia = updatedSearchResults.findIndex(
+          (item) => item.id === id
+        );
+
+        updatedSearchResults[indexOfCurrentAddedMedia] = {
+          ...updatedSearchResults[indexOfCurrentAddedMedia],
+          addedToFavorites: true,
+        };
+
+        setSearchResults(updatedSearchResults);
+      } else if (similarMovieView) {
+        let updatedSimilarMedias = [...similarMedias];
+        const indexOfCurrentAddedMedia = updatedSimilarMedias.findIndex(
+          (item) => item.id === id
+        );
+
+        updatedSimilarMedias[indexOfCurrentAddedMedia] = {
+          ...updatedSimilarMedias[indexOfCurrentAddedMedia],
+          addedToFavorites: true,
+        };
+
+        setSimilarMedias(updatedSimilarMedias);
+      } else {
+        let updatedMediaData = [...mediaData];
+
+        const findIndexOfRowItem = updatedMediaData.findIndex(
+          (item) => item.title === title
+        );
+
+        let currentMovieArrayFromRowItem =
+          updatedMediaData[findIndexOfRowItem].medias;
+        const findIndexOfCurrentMovie = currentMovieArrayFromRowItem.findIndex(
+          (item) => item.id === id
+        );
+
+        currentMovieArrayFromRowItem[findIndexOfCurrentMovie] = {
+          ...currentMovieArrayFromRowItem[findIndexOfCurrentMovie],
+          addedToFavorites: true,
+        };
+
+        setMediaData(updatedMediaData);
       }
     }
 
-    extractFavorites();
-  }, [loggedInAccount]);
+    console.log(data, "sangam");
+  }
 
-  if (session === null) return <UnauthPage />;
-  if (loggedInAccount === null) return <ManageAccounts />;
-  if (pageLoader) return <CircleLoader />;
+  async function handleRemoveFavorites(item) {
+    const res = await fetch(`/api/favorites/remove-favorite?id=${item._id}`, {
+      method: "DELETE",
+    });
+
+    const data = await res.json();
+
+    if (data.success) updateFavorites();
+  }
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
     >
-      <Navbar />
-      <div className="mt-[100px] space-y-0.5 md:space-y-2 px-4">
-        <h2 className="cursor-pointer text-sm font-semibold text-[#e5e5e5] transition-colors duration-200 hover:text-white md:text-2xl">
-          My List
-        </h2>
-        <div className="grid grid-cols-5 gap-3 items-center scrollbar-hide md:p-2">
-          {favorites && favorites.length
-            ? favorites.map((searchItem) => (
-                <MediaItem
-                  key={searchItem.id}
-                  media={searchItem}
-                  listView={true}
-                />
-              ))
-            : 'No favorites added'}
+      <div className="relative cardWrapper h-28 min-w-[180px] cursor-pointer md:h-36 md:min-w-[260px] transform transition duration-500 hover:scale-110 hover:z-[999]">
+        <Image
+          src={`${baseUrl}${media?.backdrop_path || media?.poster_path}`}
+          alt="Media"
+          layout="fill"
+          className="rounded sm object-cover md:rounded hover:rounded-sm"
+          onClick={() =>
+            router.push(
+              `/watch/${media?.type}/${listView ? media?.movieID : media?.id}`
+            )
+          }
+        />
+        <div className="space-x-3 hidden absolute p-2 bottom-0 buttonWrapper">
+          <button
+            onClick={
+              media?.addedToFavorites
+                ? listView
+                  ? () => handleRemoveFavorites(media)
+                  : null
+                : () => handleAddToFavorites(media)
+            }
+            className={`${
+              media?.addedToFavorites && !listView && "cursor-not-allowed"
+            } cursor-pointer border flex p-2 items-center gap-x-2 rounded-full  text-sm font-semibold transition hover:opacity-90 border-white   bg-black opacity-75 text-black`}
+          >
+            {media?.addedToFavorites ? (
+              <CheckIcon color="#ffffff" className="h-7 w-7" />
+            ) : (
+              <PlusIcon color="#ffffff" className="h-7 w-7" />
+            )}
+          </button>
+          <button
+            onClick={() => {
+              setShowDetailsPopup(true);
+              setCurrentMediaInfoIdAndType({
+                type: media?.type,
+                id: listView ? media?.movieID : media?.id,
+              });
+            }}
+            className="cursor-pointer p-2 border flex items-center gap-x-2 rounded-full  text-sm font-semibold transition hover:opacity-90  border-white  bg-black opacity-75 "
+          >
+            <ChevronDownIcon color="#fffffff" className="h-7 w-7" />
+          </button>
         </div>
       </div>
     </motion.div>
